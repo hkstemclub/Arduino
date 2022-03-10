ad35ccad4707c3e94864e35999a5e823386a42bf
Kit <33532829+hkstemclub@users.noreply.github.com>
2022-03-10 15:54:42 +0800

    Create 00_loop.ino

---
+++ FastLED/00_loop.ino
@@ -0,0 +1,28 @@
+#include <FastLED.h>
+
+// How many leds in your strip?
+#define NUM_LEDS 60
+
+// For led chips like WS2812, which have a data line, ground, and power, you just
+// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
+// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
+// Clock pin only needed for SPI based chipsets when not using hardware SPI
+#define DATA_PIN 3
+#define CLOCK_PIN 13
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+
+void setup() { 
+    FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);  // GRB ordering is assumed
+}
+
+    void loop() {
+        for(int dot = 0; dot < NUM_LEDS; dot++) { 
+            leds[dot] = CRGB::Blue;
+            FastLED.show();
+            // clear this led for the next time around the loop
+            leds[dot] = CRGB::Black;
+            delay(30);
+        }
+    }
